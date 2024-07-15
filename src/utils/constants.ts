c41c02d019b9c062ff1865118a8d867584b6dbdd
Pawel <pgawlik@opera.com>
2024-07-15 13:03:26 +0200

    basic page

---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+export const LINKS = {
+  github: "https://github.com/PawelGawlikDev",
+  linkedin: "https://www.linkedin.com/in/paweł-gawlik-908846234/",
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
