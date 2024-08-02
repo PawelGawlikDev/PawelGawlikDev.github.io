9e12f6da827035d32e4bbc402196a72d79bab605
Pawel <pgawlik@opera.com>
2024-08-02 16:26:49 +0200

    more fetures

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -105,7 +105,7 @@ export const Tooltip = ({
               src={item.image}
               alt={item.name}
               style={{ backgroundColor: item.background }}
-              className="object-cover !m-0 !p-0 object-top rounded-full h-14 w-14 border-2 group-hover:scale-105 group-hover:z-30 border-primary-400 relative transition duration-500"
+              className="object-cover !m-0 !p-0 object-top rounded-full h-16 w-16 border-2 group-hover:scale-105 group-hover:z-30 border-primary-400 relative transition duration-500"
             />
           </a>
         </div>
