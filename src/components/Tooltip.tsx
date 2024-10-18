1dbef13a8f04850c4b35b87ef8db56078df821d0
Paweł Gawlik <pgawlik215@gmail.com>
2024-10-18 19:58:05 +0200

    use static image in tooltip

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -7,6 +7,8 @@ import {
   useSpring,
 } from "framer-motion";
 
+import me from "../../public/assets/me.jpg";
+
 export const Tooltip = ({
   items,
 }: {
@@ -29,7 +31,6 @@ export const Tooltip = ({
   const springConfig = { stiffness: 100, damping: 5 };
   const x = useMotionValue(0);
 
-  // Rotate the tooltip
   const rotate = useSpring(
     useTransform(x, [-100, 100], [-45, 45]),
     springConfig
@@ -102,7 +103,7 @@ export const Tooltip = ({
               onMouseMove={handleMouseMove}
               height={100}
               width={100}
-              src={item.image}
+              src={item.image === "me.png" ? me.src : item.image}
               alt={item.name}
               style={{ backgroundColor: item.background }}
               className="object-cover !m-0 !p-0 object-top rounded-full h-16 w-16 border-2 group-hover:scale-105 group-hover:z-30 border-primary-400 relative transition duration-500"
